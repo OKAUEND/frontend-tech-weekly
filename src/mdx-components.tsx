04221cdb611f4ca9640b00be475c6816e7989758
OKAUEND <def1f23sbb@gmail.com>
2024-11-09 22:09:16 +0900

    変更 - 罫線を描画可能にするため、設定を追加する

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,18 @@
-import type { MDXComponents } from 'mdx/types'
- 
+import type { MDXComponents } from "mdx/types";
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
-  }
+    hr: () => (
+      <hr
+        style={{
+          margin: "2rem 0",
+          height: "2px",
+          border: "none",
+          width: "100%",
+          backgroundColor: "#e5e7eb",
+        }}
+      />
+    ),
+  };
 }
