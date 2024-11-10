1f9f3c842a905e6482bfb6760885ae56196dc295
OKAUEND <def1f23sbb@gmail.com>
2024-11-10 21:00:07 +0900

    削除 - 不要になった環境周りのファイルを削除

--- playwright/index.tsx
+++
@@ -1,5 +0,0 @@
-// playwright/index.tsx
-import { setProjectAnnotations } from "@storybook/react"; // or @storybook/vue3
-import previewAnnotations from "../.storybook/preview";
-
-setProjectAnnotations(previewAnnotations);
