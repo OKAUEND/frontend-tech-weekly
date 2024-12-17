ab58a27606dc6ba5da5e39664e5634102e3ab612
OKAUEND <def1f23sbb@gmail.com>
2024-12-17 21:13:28 +0900

    削除 - 不要なファイルを削除

--- src/feature/top/tests/Top.stories.portable.tsx
+++
@@ -1,7 +0,0 @@
-// RestaurantDetailPage.stories.portable.ts
-import { composeStories } from '@storybook/react'
-
-import * as stories from "./Top.stories"
-
-// Output an object that maps 1:1 to your stories, now in portable components
-export default composeStories(stories)
